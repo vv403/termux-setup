68cab69fd1027663df8c7adb730a917ff89ce471
vv403 <169216522+vv403@users.noreply.github.com>
2024-05-22 21:41:26 +0100

    Add Python setup

--- setup.sh
+++ setup.sh
@@ -16,3 +16,11 @@ busybox --install -s ~/.local/bin/
 tar -xvf "${HOME}/storage/downloads/ssh.tar.gz"
 
 curl https://github.com/web-flow.gpg | gpg --import
+
+python -m pip install --requirement "${0%/*}/requirements.txt" --user
+
+# https://github.com/termux/termux-packages/issues/20039#issuecomment-2096494418
+_file="$(find $PREFIX/lib/python3.11 -name "_sysconfigdata*.py")"
+rm -rf $PREFIX/lib/python3.11/__pycache__
+cp $_file "$_file".backup
+sed -i 's|-fno-openmp-implicit-rpath||g' "$_file"
